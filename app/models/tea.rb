87d1ba48ef19e5d12173439f23d6dffaf1cc4263
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-11-08 09:34:36 -0600

    Add validations for Tea model

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,4 +1,11 @@
 class Tea < ApplicationRecord 
+  validates_presence_of :name
+  validates_presence_of :description
+  validates_presence_of :temperature
+  validates :temperature, numericality: true 
+  validates_presence_of :brew_time, numericality: true 
+  validates :brew_time, numericality: true 
+  
   has_many :tea_subs
   has_many :subscriptions, through: :tea_subs
 end
