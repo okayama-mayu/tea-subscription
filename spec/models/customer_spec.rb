13775c4818fdea65465eb9334d11caf227845b39
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-11-08 09:36:21 -0600

    Add validations to Customer model

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper' 
 
 RSpec.describe Customer, type: :model do 
+  describe 'validations' do 
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:address) }
+  end
+  
   describe 'relationships' do 
     it { should have_many :customer_subscriptions }
     it { should have_many(:subscriptions).through(:customer_subscriptions) }
