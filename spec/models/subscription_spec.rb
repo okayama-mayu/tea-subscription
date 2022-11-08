99351606d2ae7eb8b9b608f914ac04e5c7e3391a
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-11-08 09:57:34 -0600

    Remove numericality validation for sstatus and frequency

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -6,9 +6,7 @@ RSpec.describe Subscription, type: :model do
     it { should validate_presence_of :price }
     it { should validate_numericality_of :price }
     it { should validate_presence_of :status }
-    it { should validate_numericality_of :status }
     it { should validate_presence_of :frequency }
-    it { should validate_numericality_of :frequency }
   end
   
   describe 'relationships' do 
