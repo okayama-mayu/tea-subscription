4fed70d7afdc8dc9313c4166c7c11fbd47888fc1
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-11-08 14:37:28 -0600

    Remove validation for numericality of status

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,12 +3,11 @@ class Subscription < ApplicationRecord
   validates_presence_of :price 
   validates :price, numericality: true 
   validates_presence_of :status 
-  validates :status, numericality: true 
   validates_presence_of :frequency
   validates :frequency, numericality: true 
 
   enum status: { active: 0, cancelled: 1 }
-  enum frequency: { biweekly: 0, monthly: 1, quaterly: 2 }
+  enum frequency: { biweekly: 0, monthly: 1, quaterly: 2, one_time: 3 }
 
   belongs_to :tea 
   belongs_to :customer
