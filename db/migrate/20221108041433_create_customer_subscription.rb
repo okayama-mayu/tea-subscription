b00906772a93d009a244cfc64db8a8dc12178cb7
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-11-08 12:29:29 -0600

    Update schema to make Subscription the joins table

--- db/migrate/20221108041433_create_customer_subscription.rb
+++
@@ -1,8 +0,0 @@
-class CreateCustomerSubscription < ActiveRecord::Migration[5.2]
-  def change
-    create_table :customer_subscriptions do |t|
-      t.references :customer, foreign_key: true
-      t.references :subscription, foreign_key: true
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_08_151616) do
+ActiveRecord::Schema.define(version: 2022_11_08_181622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "customer_subscriptions", force: :cascade do |t|
-    t.bigint "customer_id"
-    t.bigint "subscription_id"
-    t.index ["customer_id"], name: "index_customer_subscriptions_on_customer_id"
-    t.index ["subscription_id"], name: "index_customer_subscriptions_on_subscription_id"
-  end
 
   create_table "customers", force: :cascade do |t|
     t.string "first_name"
@@ -38,13 +31,10 @@ ActiveRecord::Schema.define(version: 2022_11_08_151616) do
     t.integer "frequency"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "tea_subs", force: :cascade do |t|
     t.bigint "tea_id"
-    t.bigint "subscription_id"
-    t.index ["subscription_id"], name: "index_tea_subs_on_subscription_id"
-    t.index ["tea_id"], name: "index_tea_subs_on_tea_id"
+    t.bigint "customer_id"
+    t.index ["customer_id"], name: "index_subscriptions_on_customer_id"
+    t.index ["tea_id"], name: "index_subscriptions_on_tea_id"
   end
 
   create_table "teas", force: :cascade do |t|
@@ -56,8 +46,6 @@ ActiveRecord::Schema.define(version: 2022_11_08_151616) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "customer_subscriptions", "customers"
-  add_foreign_key "customer_subscriptions", "subscriptions"
-  add_foreign_key "tea_subs", "subscriptions"
-  add_foreign_key "tea_subs", "teas"
+  add_foreign_key "subscriptions", "customers"
+  add_foreign_key "subscriptions", "teas"
 end
