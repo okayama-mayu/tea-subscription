5b5186d8c2e79ada666641f9e85e7dc3fdb5df0d
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-11-08 09:40:47 -0600

    Add validations to Subscription model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,12 @@
 class Subscription < ApplicationRecord
+  validates_presence_of :title 
+  validates_presence_of :price 
+  validates :price, numericality: true 
+  validates_presence_of :status 
+  validates :status, numericality: true 
+  validates_presence_of :frequency
+  validates :frequency, numericality: true 
+
   has_many :tea_subs
   has_many :customer_subscriptions
   has_many :customers, through: :customer_subscriptions
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper' 
 
 RSpec.describe Subscription, type: :model do 
+  describe 'validations' do 
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :price }
+    it { should validate_numericality_of :price }
+    it { should validate_presence_of :status }
+    it { should validate_numericality_of :status }
+    it { should validate_presence_of :frequency }
+    it { should validate_numericality_of :frequency }
+  end
+  
   describe 'relationships' do 
     it { should have_many :tea_subs }
     it { should have_many :customer_subscriptions }
