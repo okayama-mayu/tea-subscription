949b3f06b6f82c853d01e5030abb6ea9484f6c74
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-11-09 22:43:43 -0600

    Hard code ids in seeds file for Postman reference

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 # customers 
-mayu = Customer.create!(first_name: 'Mayu', last_name: 'Takeda', email: 'mayu@email.com', address: 'New Orleans, LA')
-nicole = Customer.create!(first_name: 'Nicole', last_name: 'Esquer', email: 'nicole@email.com', address: 'Tuscon, AZ')
+mayu = Customer.create!(id: 1, first_name: 'Mayu', last_name: 'Takeda', email: 'mayu@email.com', address: 'New Orleans, LA')
+nicole = Customer.create!(id: 2, first_name: 'Nicole', last_name: 'Esquer', email: 'nicole@email.com', address: 'Tuscon, AZ')
 
 # teas
-green = Tea.create!(name: 'green', description: 'it is good', temperature: 95, brew_time: 3)
-earl = Tea.create!(name: 'earl grey', description: 'it is strong', temperature: 80, brew_time: 4)
+green = Tea.create!(id: 1, name: 'green', description: 'it is good', temperature: 95, brew_time: 3)
+earl = Tea.create!(id: 2, name: 'earl grey', description: 'it is strong', temperature: 80, brew_time: 4)
 
 # subscriptions 
-nicole_green = Subscription.create!(title: 'Nicoles Green tea subscription', price: 10, frequency: 3, tea_id: green.id, customer_id: nicole.id)
+nicole_green = Subscription.create!(id: 1, title: 'Nicoles Green tea subscription', price: 10, frequency: 3, tea_id: green.id, customer_id: nicole.id)
